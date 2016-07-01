282d35514a4c2475be36f4ab58a5c317c3171460
Josef Rousek <josef.rousek@gmail.com>
2016-07-01 12:36:29 +0200

    Setup and fix tests

--- test/util_test.js
+++ test/util_test.js
@@ -10,14 +10,15 @@ describe('util', function() {
 		});
 
 		it('should return false if email is not valid', function() {
-			assert.notOk(util.isEmail('development@brandembassy.com'));
+			assert.notOk(util.isEmail('development@brandembassy'));
 		});
 	});
 
 	describe('isPhone', function() {
 
-		it('should return true if phone number has 9 digits');
-
+		it('should return true if phone number has 9 digits', function() {
+			assert.ok(util.isPhone('608123456'));
+		});
 	});
 
 });
