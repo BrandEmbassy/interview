379bde881d27abfc13d469a95e4c72bf7639409a
Vaclav Koldus <vaclav.koldus@ncr.com>
2016-07-10 12:00:41 +0200

    util test fixed, added coverage

--- test/util_test.js
+++ test/util_test.js
@@ -9,15 +9,59 @@ describe('util', function() {
 			assert.ok(util.isEmail('development@brandembassy.com'));
 		});
 
-		it('should return false if email is not valid', function() {
-			assert.notOk(util.isEmail('development@brandembassy.com'));
+		it('should return false if email lacks @', function() {
+			assert.notOk(util.isEmail('developmentbrandembassy.com'));
 		});
+
+		it('should return false if email lacks domain suffic', function() {
+			assert.notOk(util.isEmail('development@brandembassycom'));
+		});
+
+		it('should return false if email lacks recognized domain suffic', function() {
+			assert.notOk(util.isEmail('development@brandembassy.invalid'));
+		});
+
+		it('should return false if null passed in', function() {
+			assert.notOk(util.isEmail(null));
+		});
+
+		it('should return false if nothing passed in', function() {
+			assert.notOk(util.isEmail());
+		});
+
+		it('should return false if undefined passed in', function() {
+			var undef;
+			assert.notOk(util.isEmail(undef));
+		});
+
 	});
 
 	describe('isPhone', function() {
 
-		it('should return true if phone number has 9 digits');
+		it('should return true if phone number has 9 digits', function() {
+			assert.ok(util.isPhone("123456789"))
+		});
 
+		it('should return false if phone number doesn\'t have 9 digits', function() {
+			assert.notOk(util.isPhone("1234567890"))
+		});
+
+		it('should return false if phone number contains non-numerical characters', function() {
+			assert.notOk(util.isPhone("12345678a"))
+		});
+
+		it('should return false if null passed in', function() {
+			assert.notOk(util.isPhone(null));
+		});
+
+		it('should return false if nothing passed in', function() {
+			assert.notOk(util.isPhone());
+		});
+
+		it('should return false if undefined passed in', function() {
+			var undef;
+			assert.notOk(util.isPhone(undef));
+		});
 	});
 
 });
