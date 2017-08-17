98e18a859ba0a0abec0fdd925f2140db88a2abb3
David Vavrousek <david.vavrousek@massive.co>
2017-08-17 15:27:35 +0200

    test deploy

--- .circleci/deploy-heroku.sh
+++ .circleci/deploy-heroku.sh
@@ -4,4 +4,4 @@ echo "Deploy app - $1"
 
 [[ ! -s \"$(git rev-parse --git-dir)/shallow\" ]] || git fetch --unshallow
 
-git subtree push --prefix dist -f git@heroku.com:$1.git $CIRCLE_SHA1:refs/heads/master
+git subtree push --prefix dist git@heroku.com:$1.git $CIRCLE_SHA1:refs/heads/master
