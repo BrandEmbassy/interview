d35734c94e0f7240208879d0e85be23662d28360
David Vavrousek <david.vavrousek@massive.co>
2017-08-17 16:03:26 +0200

    remove f

--- .circleci/deploy-heroku.sh
+++ .circleci/deploy-heroku.sh
@@ -8,4 +8,4 @@ rm .gitignore
 git add dist
 git commit -m "add dist"
 
-git subtree push -f --prefix=dist git@heroku.com:$1.git $CIRCLE_SHA1:refs/heads/master
+git subtree push --prefix=dist git@heroku.com:$1.git $CIRCLE_SHA1:refs/heads/master
