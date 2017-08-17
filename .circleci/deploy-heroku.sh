9e82dadaac2c5acee7722f6fff49f5336740edf4
David Vavrousek <david.vavrousek@massive.co>
2017-08-17 16:21:59 +0200

    test deploy with ignored dist

--- .circleci/deploy-heroku.sh
+++ .circleci/deploy-heroku.sh
@@ -3,5 +3,6 @@
 echo "Deploy app - $1"
 
 git add dist -f
+git commit -m "add dist"
 
-git push git@heroku.com:$1.git `git subtree split --prefix dist master`:$CIRCLE_SHA1:refs/heads/master --force
+git push -f git@heroku.com:$1.git $CIRCLE_SHA1:refs/heads/master
