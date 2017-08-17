a4ec73e820fbab7cc7cf11d3949d70edebb9e518
David Vavrousek <david.vavrousek@massive.co>
2017-08-17 16:00:46 +0200

    push just subtree

--- .circleci/deploy-heroku.sh
+++ .circleci/deploy-heroku.sh
@@ -4,4 +4,8 @@ echo "Deploy app - $1"
 
 [[ ! -s \"$(git rev-parse --git-dir)/shallow\" ]] || git fetch --unshallow
 
-git push -f git@heroku.com:$1.git $CIRCLE_SHA1:refs/heads/master
+rm .gitignore
+git add dist
+git commit -m "add dist"
+
+git subtree push -f --prefix=dist git@heroku.com:$1.git $CIRCLE_SHA1:refs/heads/master
