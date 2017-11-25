8b879069b4dff6a06af0d3a447db1fdb75a63d6b
Maurizio Pucci <devmao@gmail.com>
2017-11-25 18:37:04 +0100

    test(): add storybook (WIP)

--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -1,44 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import Input from './Input';
 
+class TestAttrValueAndDefaultValue extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 1,
+      value: 'Ciao!'
+    };
+  }
+
+  changeValue = (event) => {
+    this.setState((state, props) => ({
+      value: `New Value ${state.counter}`,
+      counter: state.counter + 1
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Attr value</h3>
+        <Input value={this.state.value} />
+        <h3></h3>
+        <h3>Attr defaultValue</h3>
+        <Input defaultValue={this.state.value} />
+        <p>
+          <button
+            onClick={this.changeValue}>
+            Change value and defaultValue
+            </button>
+        </p>
+      </div>
+    );
+  }
+}
+
 storiesOf('Input', module)
-  .add('with default', () => (
+  .add('No attrs', () => (
     <Input />
   ))
-  .add('with number type', () => (
-    <Input type="number" />
+  .add('Attr required', () => (
+    <Input required />
   ))
-  .add('with datetime type', () => (
+  .add('Attr type to datetime-local', () => (
     <Input type="datetime-local" />
   ))
-  .add('with email type', () => (
-    <Input type="email" />
-  ))
-  .add('with autofocus', () => (
+  .add('Attr autofocus', () => (
     <Input autofocus />
   ))
-  .add('with placeholder', () => (
+  .add('Attr placeholder', () => (
     <Input placeholder="Enter something!" />
   ))
-  .add('with custom className', () => (
+  .add('Attr className', () => (
     <Input className="my-custom-class" />
   ))
-  .add('with value', () => (
-    <Input value="Ciao gente!" />
+  .add('Attr value and defaultValue', () => (
+    <TestAttrValueAndDefaultValue />
   ))
-  .add('with default value', () => (
-    <Input defaultValue="I'm the default value" />
-  ))
-  .add('function focus()', () => {
+  .add('API focus', () => {
     return (
       <div>
         <Input ref={(elem) => this.inputElem = elem} />
         <p>
           <button
             onClick={() => this.inputElem.focus()}>
-            call focus()
+            Focus me!
           </button>
         </p>
       </div>
